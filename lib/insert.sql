ae7f21e0e4984850c04fb593b3bb7a87d05249b2
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2022-11-06 13:38:14 -0700

    commit tree 1e2c6e444fd7b59d83598c556517da5bd1c15652

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO bears (name)
+VALUES ('Mr. Chocolate', 'Rowdy', 'Tabitha', 'Sergeant Brown', 'Melissa', 'Grinch', 'Wendy', null);
+
+INSERT INTO bears (sex)
+VALUES ('M', 'M', 'F', 'M', 'F', 'M', 'F', 'M');
+
